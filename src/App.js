50c473da7c55e0c96ea8d1e0dd6ec94785cecfbf
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-20 19:15:33 +0530

    Modify src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quote';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <Quotes />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quotes" element={<Quotes />} />
+      </Routes>
     </>
   );
 }
