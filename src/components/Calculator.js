7349c9f21122e9f8657caee3f508c054404e522d
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-20 19:17:27 +0530

    Modify src/components/Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,40 +15,43 @@ function Calculator() {
     setResult(calculate(result, val));
   };
   return (
-    <div className="calc">
-      <div className="input">
-        {next || total || operation || 0}
+    <>
+      <div className="calcText">
+        <h2>Let&apos;s do some math!</h2>
+        <div className="calc">
+          <div className="input">{next || total || operation || 0}</div>
+          <div className="row">
+            <Button text="AC" handleClick={handleClick} />
+            <Button text="+/-" handleClick={handleClick} />
+            <Button text="%" handleClick={handleClick} />
+            <Button text="÷" handleClick={handleClick} />
+          </div>
+          <div className="row">
+            <Button text="7" handleClick={handleClick} />
+            <Button text="8" handleClick={handleClick} />
+            <Button text="9" handleClick={handleClick} />
+            <Button text="x" handleClick={handleClick} />
+          </div>
+          <div className="row">
+            <Button text="4" handleClick={handleClick} />
+            <Button text="5" handleClick={handleClick} />
+            <Button text="6" handleClick={handleClick} />
+            <Button text="-" handleClick={handleClick} />
+          </div>
+          <div className="row">
+            <Button text="1" handleClick={handleClick} />
+            <Button text="2" handleClick={handleClick} />
+            <Button text="3" handleClick={handleClick} />
+            <Button text="+" handleClick={handleClick} />
+          </div>
+          <div className="row">
+            <Button text="0" handleClick={handleClick} />
+            <Button text="." handleClick={handleClick} />
+            <Button text="=" handleClick={handleClick} />
+          </div>
+        </div>
       </div>
-      <div className="row">
-        <Button text="AC" handleClick={handleClick} />
-        <Button text="+/-" handleClick={handleClick} />
-        <Button text="%" handleClick={handleClick} />
-        <Button text="÷" handleClick={handleClick} />
-      </div>
-      <div className="row">
-        <Button text="7" handleClick={handleClick} />
-        <Button text="8" handleClick={handleClick} />
-        <Button text="9" handleClick={handleClick} />
-        <Button text="x" handleClick={handleClick} />
-      </div>
-      <div className="row">
-        <Button text="4" handleClick={handleClick} />
-        <Button text="5" handleClick={handleClick} />
-        <Button text="6" handleClick={handleClick} />
-        <Button text="-" handleClick={handleClick} />
-      </div>
-      <div className="row">
-        <Button text="1" handleClick={handleClick} />
-        <Button text="2" handleClick={handleClick} />
-        <Button text="3" handleClick={handleClick} />
-        <Button text="+" handleClick={handleClick} />
-      </div>
-      <div className="row">
-        <Button text="0" handleClick={handleClick} />
-        <Button text="." handleClick={handleClick} />
-        <Button text="=" handleClick={handleClick} />
-      </div>
-    </div>
+    </>
   );
 }
 
